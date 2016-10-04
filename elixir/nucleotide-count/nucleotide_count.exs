4ad4ac2781061e1db30d2a4b63ae8afc219d1ffe
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2016-10-05 02:30:28 +0300

    Use `Map.new`

--- elixir/nucleotide-count/nucleotide_count.exs
+++ elixir/nucleotide-count/nucleotide_count.exs
@@ -28,11 +28,6 @@ defmodule NucleotideCount do
   """
   @spec histogram([char]) :: map
   def histogram(strand) do
-    %{
-      ?A => count(strand, ?A),
-      ?T => count(strand, ?T),
-      ?C => count(strand, ?C),
-      ?G => count(strand, ?G)
-    }
+    Map.new(@nucleotides, &({&1, count(strand, &1)}))
   end
 end
