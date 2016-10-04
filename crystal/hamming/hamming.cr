76bff8945b478deb88f75a38261e78d7e107b1a6
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2016-10-05 01:29:12 +0300

    Added crystal/hamming solution

---
+++ crystal/hamming/hamming.cr
@@ -0,0 +1,10 @@
+module Hamming
+  def self.compute(sequence1 : String, sequence2 : String)
+    if sequence1.size != sequence2.size
+      raise ArgumentError.new("sequences must have equal size")
+    end
+    sequence1.chars.zip(sequence2.chars).select do |pair|
+      pair[0] != pair[1]
+    end.size
+  end
+end
