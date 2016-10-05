bd8564de6dc2bed6a816b6d04ee004ab2273eba6
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2016-10-06 00:18:37 +0300

    [crystal/rna_transcription] Use `String#tr`

--- crystal/rna-transcription/rna_transcription.cr
+++ crystal/rna-transcription/rna_transcription.cr
@@ -1,12 +1,5 @@
 module RnaComplement
-  COMPLEMENTS = {
-    'G' => 'C',
-    'C' => 'G',
-    'T' => 'A',
-    'A' => 'U'
-  }
-
   def self.of_dna(strand : String) : String
-    strand.chars.map { |nucleotide| COMPLEMENTS[nucleotide] }.join
+    strand.tr("GCTA", "CGAU")
   end
 end
